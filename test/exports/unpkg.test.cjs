9c8b106b4cbc76f561018e37dd34a2851985e1fd
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-11 03:24:07 -0800

    Upgrade dependencies

--- test/exports/unpkg.test.cjs
+++ test/exports/unpkg.test.cjs
@@ -1,9 +1,8 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 const assert = require('assert');
 const { Active, ActiveBoundary } = require('react-native-outside/dist/umd/react-native-outside.min.js');
 
-describe('exports react-native-outside/dist/umd/react-native-outside.js', function () {
-  it('defaults', function () {
+describe('exports react-native-outside/dist/umd/react-native-outside.js', () => {
+  it('defaults', () => {
     assert.equal(typeof Active, 'function');
     assert.equal(typeof ActiveBoundary, 'function');
   });
