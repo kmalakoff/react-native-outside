b5149927182667933fb7b9a012d48ac769a45d0f
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-17 12:03:28 -0800

    Upgrade modules

--- wtr.config.mjs
+++ wtr.config.mjs
@@ -2,7 +2,7 @@ import { importMapsPlugin } from '@web/dev-server-import-maps';
 import createConfig from 'tsds-web-test-runner/createConfig.mjs';
 
 export default createConfig({
-  port: 8011,
+  port: 9012,
   plugins: [
     importMapsPlugin({
       inject: {
