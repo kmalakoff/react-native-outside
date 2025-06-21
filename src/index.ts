43101d56c2f5cf9771bd2f9372d1fc582a65ce0c
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-21 13:22:26 -0700

    Comprehensive types

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { default as Active } from './Active.ts';
 export { default as ActiveBoundary } from './ActiveBoundary.ts';
+export * from './types.ts';
---
+++ src/types.ts
@@ -0,0 +1,3 @@
+export interface ActiveBoundaryProps {
+  children: React.ReactNode;
+}
